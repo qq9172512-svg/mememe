4fa255c78826e1a681eb104e1bf3cbf4081dd619
qq9172512-svg <qq9172512@gmail.com>
2026-08-09 18:00:05 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'broker-study-app-v2';
+const CACHE_NAME = 'broker-study-app-v3';
 const ASSETS = [
   './',
   './app.html',
@@ -10,8 +10,19 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
