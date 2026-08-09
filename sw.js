02e5d64788714fb6ced63eef9aacf4ebb7e790a8
qq9172512-svg <qq9172512@gmail.com>
2026-08-09 19:20:41 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,33 @@
-const CACHE_NAME="broker-app-v1"; self.addEventListener("install",()=>self.skipWaiting()); self.addEventListener("activate",e=>e.waitUntil(self.clients.claim())); self.addEventListener("fetch",()=>{});
+const CACHE_NAME = 'broker-study-app-v3';
+const ASSETS = [
+  './',
+  './app.html',
+  './index.html',
+  './manifest.json',
+  './data/crash35_data.js',
+  './data/flashcards.js',
+  './data/questions.js'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
